0c7fa43bc32b1fe4d2def5c9e0781f712776527f
Tom Cornall <cornall.tom@gmail.com>
2016-04-03 22:05:15 -0700

    YE initial brrrrrrrrrrraw

--- scripts/apptest.js
+++ scripts/apptest.js
@@ -1,18 +1,18 @@
+
 console.log("hi");
 $posY=0;
-$vy = 1;
+$vy = 0.01;
+$trigger = false;
 var mousedownID = -1;
 $(document).ready(function() {
 	$('#tom').mousedown(function() {
-	  // $('#dot').addClass("pinball");
-	  // $posY += $vy
-	  // $('#dot').css({bottom:$posY + 'px'});
+
 	  if(mousedownID==-1) { //Prevent multimple loops!
 			mousedownID = setInterval(whilemousedown, 10 /*execute every 100ms*/);
 	  }
 	});
 	$('#tom').mouseup(function() {
-	 //  $('#dot').removeClass("pinball");
+
 		if(mousedownID!=-1) {  //Only stop if exists
 			clearInterval(mousedownID);
 			mousedownID=-1;
@@ -27,18 +27,20 @@ $(document).ready(function() {
 	});
 
 	function whilemousedown() {
-		if ($posY <= 0) {
-			$vy += gravity;
-		}
-	  console.log(mousedownID);
-	  $posY += $vy
+		if ($vy > 2) {
+			$trigger = true;
+	  	$vy -= gravity;
+	  }
+	  if ($trigger = false) {
+	  	$vy += gravity;
+	  }
+
+		console.log($vy);
+	  $posY += $vy;
+
 	  $('#dot').css({bottom:$posY + 'px'});
 	}
 });
 
-// var G = 9.8;
-// var botY = 0;
-// ball.y += vy;
-var gravity = 0.05;
-
+var gravity = 0.035;
 
