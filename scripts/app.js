6b60b77980f833dd1211c877690429fe32119206
Tom Cornall <cornall.tom@gmail.com>
2016-04-04 21:02:57 -0700

    JS for PROPER responsivity...:100:

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(window).resize(function() {
+		console.log("resize");
+	  if ($(window).width() < $(window).height() && $(window).width() < 600) {
+	  	$("#wrap").addClass("width-is-the-decider");
+	  	$("#wrap").removeClass("height-is-the-decider");
+			console.log("width decider");
+	  } else if($(window).height() < 600) {
+	  	$("#wrap").addClass("height-is-the-decider");
+	  	$("#wrap").removeClass("width-is-the-decider");
+	  } else {
+	  	$("#wrap").removeClass("width-is-the-decider");
+	  	$("#wrap").removeClass("height-is-the-decider");
+	  }
+	});
+	$(window).resize();
+});
